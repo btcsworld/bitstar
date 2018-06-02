ed6a0a72467c4abe1386825147467ab66dcea7db
btcsworld <39853172+btcsworld@users.noreply.github.com>
2018-06-02 19:05:50 +0800

    bitstar
    bitstar global world

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Bitstar-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BitstarURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
